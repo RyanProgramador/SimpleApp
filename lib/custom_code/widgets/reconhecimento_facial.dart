17fbdfc2c3de646778984c958540b445829d6c5e
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-01-25 19:01:51 +0000

    Updating to latest FlutterFlow output.

--- lib/custom_code/widgets/reconhecimento_facial.dart
+++ lib/custom_code/widgets/reconhecimento_facial.dart
@@ -8,14 +8,14 @@ import 'package:flutter/material.dart';
 // DO NOT REMOVE OR MODIFY THE CODE ABOVE!
 
 import 'dart:io';
-import 'dart:ui' as ui;
-import 'package:camera/camera.dart';
-import 'package:google_mlkit_face_detection/google_mlkit_face_detection.dart';
-import 'package:path_provider/path_provider.dart';
+import 'package:face_camera/face_camera.dart';
 
 class ReconhecimentoFacial extends StatefulWidget {
-  const ReconhecimentoFacial({Key? key, this.width, this.height})
-      : super(key: key);
+  const ReconhecimentoFacial({
+    Key? key,
+    this.width,
+    this.height,
+  }) : super(key: key);
 
   final double? width;
   final double? height;
@@ -25,137 +25,44 @@ class ReconhecimentoFacial extends StatefulWidget {
 }
 
 class _ReconhecimentoFacialState extends State<ReconhecimentoFacial> {
-  CameraController? _cameraController;
-  List<CameraDescription>? cameras;
-  XFile? _imageFile;
-  List<Face> _faces = [];
-  ui.Image? _image;
-
-  @override
-  void initState() {
-    super.initState();
-    _initializeCamera();
-  }
-
-  Future<void> _initializeCamera() async {
-    cameras = await availableCameras();
-    if (cameras!.isNotEmpty) {
-      _cameraController =
-          CameraController(cameras![0], ResolutionPreset.medium);
-      await _cameraController!.initialize();
-      if (!mounted) return;
-      setState(() {});
-    } else {
-      print("Nenhuma câmera encontrada");
-    }
-  }
-
-  Future<void> _detectFaces(XFile file) async {
-    final inputImage = InputImage.fromFilePath(file.path);
-    final faceDetector = FaceDetector(options: FaceDetectorOptions());
-    final faces = await faceDetector.processImage(inputImage);
-    setState(() {
-      _faces = faces;
-    });
-    await _loadImage(file);
-    faceDetector.close();
-  }
-
-  Future<void> _loadImage(XFile file) async {
-    final data = await file.readAsBytes();
-    final image = await decodeImageFromList(data);
-    setState(() {
-      _image = image;
-    });
-  }
-
-  Future<void> _takePicture() async {
-    if (_cameraController == null || !_cameraController!.value.isInitialized) {
-      print('Erro: selecione uma câmera primeiro.');
-      return;
-    }
-
-    if (_cameraController!.value.isTakingPicture) {
-      // Uma captura está em progresso; não faça nada.
-      return;
-    }
-
-    try {
-      final XFile file = await _cameraController!.takePicture();
-      _detectFaces(file);
-      setState(() {
-        _imageFile = file;
-      });
-    } catch (e) {
-      print(e);
-    }
-  }
-
-  @override
-  void dispose() {
-    _cameraController?.dispose();
-    super.dispose();
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: <Widget>[
-          Expanded(
-            child: _cameraController != null &&
-                    _cameraController!.value.isInitialized
-                ? AspectRatio(
-                    aspectRatio: _cameraController!.value.aspectRatio,
-                    child: CameraPreview(_cameraController!),
-                  )
-                : Center(child: CircularProgressIndicator()),
-          ),
-          _image != null && _faces.isNotEmpty
-              ? FittedBox(
-                  child: SizedBox(
-                    width: _image!.width.toDouble(),
-                    height: _image!.height.toDouble(),
-                    child: CustomPaint(
-                      painter: FacePainter(_image!, _faces),
-                    ),
+      body: SmartFaceCamera(
+        autoCapture:
+            false, // Defina como true para captura automática quando um rosto for detectado
+        defaultCameraLens: CameraLens.front, // Use a câmera frontal
+        message: 'Centralize seu rosto no quadrado', // Mensagem de instrução
+        onCapture: (File? image) {
+          // Este callback é chamado quando uma foto é capturada
+          if (image != null) {
+            // Aqui você pode manipular a imagem capturada
+            showDialog(
+              context: context,
+              builder: (context) => AlertDialog(
+                content: Image.file(image),
+                actions: [
+                  TextButton(
+                    onPressed: () => Navigator.of(context).pop(),
+                    child: Text('Fechar'),
                   ),
-                )
-              : Container(),
-        ],
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _takePicture,
-        tooltip: 'Detectar',
-        child: Icon(Icons.camera_alt),
+                ],
+              ),
+            );
+          }
+        },
+        onFaceDetected: (DetectedFace? face) {
+          // Este callback é chamado quando um rosto é detectado
+          // Você pode usar este callback para personalizar ações baseadas na detecção de rosto
+        },
       ),
     );
   }
 }
 
-class FacePainter extends CustomPainter {
-  final ui.Image image;
-  final List<Face> faces;
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized(); // Adicione isso
+  await FaceCamera.initialize(); // E isso para inicializar a face_camera
 
-  FacePainter(this.image, this.faces);
-
-  @override
-  void paint(Canvas canvas, Size size) {
-    canvas.drawImage(image, Offset.zero, Paint());
-    for (var face in faces) {
-      final rect = face.boundingBox;
-      canvas.drawRect(
-        rect,
-        Paint()
-          ..color = Colors.red
-          ..style = PaintingStyle.stroke
-          ..strokeWidth = 5.0,
-      );
-    }
-  }
-
-  @override
-  bool shouldRepaint(covariant CustomPainter oldDelegate) {
-    return false;
-  }
+  runApp(MaterialApp(home: ReconhecimentoFacial()));
 }
