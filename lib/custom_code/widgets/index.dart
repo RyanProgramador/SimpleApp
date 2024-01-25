26b4beb2e2c7887d12a6e9a7349c6d78f68459c2
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-01-25 19:01:27 +0000

    Updating to latest FlutterFlow output.

--- lib/custom_code/widgets/index.dart
+++ lib/custom_code/widgets/index.dart
@@ -1,2 +1,3 @@
 export 'route_view_live2.dart' show RouteViewLive2;
 export 'reconhecimento_facial.dart' show ReconhecimentoFacial;
+export 'face_mesh_detector_view_state.dart' show FaceMeshDetectorViewState;
